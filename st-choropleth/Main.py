4051604bfeb5dcf9892fe383e90e4b4c36ae7ba7
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-12-26 11:02:30 +0100

    edited

--- st-choropleth/Main.py
+++ st-choropleth/Main.py
@@ -1,33 +1,20 @@
 import streamlit as st
 
+# This should be the first Streamlit command used in your app, and it should only be called once
 st.set_page_config(layout="wide")
 
 st.markdown("""
 # AI Machine Learning and decision support system
 ---
-            
-### Select the pages from the sidebar to see demonstrations of the various aspects of Plotly's map functions.
-            
----
-""")
 
-import streamlit as st
-
-st.set_page_config(layout="wide")
-
-st.markdown("""
-# Towards a GIS using Plotly and Streamlit
----
-            
 ### Select the pages from the sidebar to see demonstrations of the various aspects of Plotly's map functions.
 
-#### The app is is described and documented in the tutorial [__*How to create a GIS with Plotly and Streamlit*__](https://towardsdatascience.com/how-to-create-a-simple-gis-map-with-plotly-and-streamlit-7732d67b84e2).
-            
 ---
 """)
 
 col2, col3, col4 = st.columns(3)
-                       
-col2.image("images/1_VyS4gt40OKHc_i6FSRGIrA.png")
-col3.image("images/Screenshot 2023-03-08 194017.png")
-col4.image("images/Screenshot 2023-12-20 200434.png")
+
+# Ensure that the image paths are correct and relative to your Streamlit application's root directory
+col2.image("images/a.png")
+col3.image("images/b.png")
+col4.image("images/c.png")
