2e7095f6c647223d74ae7fea661b927c14d27d74
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-12-27 22:47:39 +0100

    edited

--- flood-mapping-tool/app/Home.py
+++ flood-mapping-tool/app/Home.py
@@ -7,6 +7,8 @@ from src.utils import (
     set_home_page_style,
     toggle_menu_button,
 )
+import requests
+from io import BytesIO
 
 # Page configuration
 st.set_page_config(layout="wide", page_title=params["browser_title"])
@@ -14,6 +16,23 @@ st.set_page_config(layout="wide", page_title=params["browser_title"])
 # If app is deployed hide menu button
 toggle_menu_button()
 
+
+def get_base64_of_bin_file(url_or_path):
+    """
+    This function will handle both URLs and local file paths.
+    It opens the file or fetches it from a URL, then converts it to a base64 string.
+    """
+    if url_or_path.startswith('http://') or url_or_path.startswith('https://'):
+        response = requests.get(url_or_path)
+        if response.status_code == 200:
+            file_content = BytesIO(response.content)
+        else:
+            raise Exception(f"Error fetching file from URL: {url_or_path}")
+    else:
+        with open(url_or_path, "rb") as f:
+            file_content = f.read()
+
+    return base64.b64encode(file_content).decode("utf-8")
 # Create sidebar
 add_logo("https://raw.githubusercontent.com/desmond-lartey/Knowledge-Management-Informatics/Fires/flood-mapping-tool/app/img/MAlogo.png")
 add_about()
