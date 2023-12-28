b47a3c43d6a6a56c62826f7f9e9a9a63410ab081
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-12-28 23:20:04 +0100

    edited

--- flood-mapping-tool/app/src/utils_ee.py
+++ flood-mapping-tool/app/src/utils_ee.py
@@ -1,58 +1,41 @@
-"""Module for ee-related functionalities."""
+"""Module for Earth Engine-related functionalities."""
 import ee
 import streamlit as st
 from ee import oauth
 from google.oauth2 import service_account
 from src.utils import is_app_on_streamlit
 
-
 @st.experimental_memo
 def ee_initialize(force_use_service_account: bool = False):
-    """Initialise Google Earth Engine.
+    """Initialize Google Earth Engine.
 
-    Checks whether the app is deployed on Streamlit Cloud and, based on the
-    result, initialises Google Earth Engine in different ways: if the app is
-    run locally, the credentials are retrieved from the user's credentials
-    stored in the local system (personal Google account is used). If the app
-    is deployed on Streamlit Cloud, credentials are taken from the secrets
-    field in the cloud (a dedicated service account is used).
-    Inputs:
-        force_use_service_account (bool): If True, the dedicated Google
-            service account is used, regardless of whether the app is run
-            locally or in the cloud. To be able to use a service account
-            locally, a file called "secrets.toml" should be added to the
-            folder ".streamlit", in the main project folder.
-
-    Returns:
-        None
-    """
-    if force_use_service_account or is_app_on_streamlit():
-        service_account_keys = st.secrets["ee_keys"]
-        credentials = service_account.Credentials.from_service_account_info(
-            service_account_keys, scopes=oauth.SCOPES
-        )
-        ee.Initialize(credentials)
-    else:
-        ee.Initialize()
-
-
-# Import necessary libraries for Earth Engine initialization
-import ee
-import streamlit as st
-from google.oauth2 import service_account
-from ee import oauth
-
-def ee_initialize(force_use_service_account: bool = False):
-    """Initialize Google Earth Engine with optional service account.
+    This function checks whether the app is deployed on Streamlit Cloud and
+    initializes Google Earth Engine accordingly. If the app runs locally,
+    personal Google account credentials are used. If deployed on Streamlit Cloud,
+    it uses credentials from the secrets field in the cloud.
 
     Args:
-        force_use_service_account (bool): If True, forces the use of a service account.
+        force_use_service_account (bool): If True, a dedicated Google service
+            account is used, regardless of whether the app is run locally or
+            in the cloud. To use a service account locally, a file named
+            "secrets.toml" should be added to the ".streamlit" folder in the
+            main project folder.
     """
-    if force_use_service_account or "ee_keys" in st.secrets:
-        service_account_keys = st.secrets["ee_keys"]
-        credentials = service_account.Credentials.from_service_account_info(
-            service_account_keys, scopes=oauth.SCOPES
-        )
-        ee.Initialize(credentials)
-    else:
-        ee.Initialize()
+    try:
+        if force_use_service_account or is_app_on_streamlit():
+            # Retrieve service account keys from Streamlit secrets
+            service_account_keys = st.secrets.get("ee_keys")
+            if service_account_keys is None:
+                raise ValueError("Service account keys not found in Streamlit secrets.")
+            credentials = service_account.Credentials.from_service_account_info(
+                service_account_keys, scopes=oauth.SCOPES
+            )
+            ee.Initialize(credentials)
+        else:
+            # Initialize with default credentials (local development)
+            ee.Initialize()
+    except Exception as e:
+        print(f"Error initializing Google Earth Engine: {e}")
+        raise
+
+# Example usage: ee_initialize(force_use_service_account=True)
